903769dcdaccb4564984eae563e16d7bd7ec4aa3
Konstantin Os <permkonstantin@gmail.com>
2023-04-06 22:14:56 +0500

    Fin_project_commit_2

--- Fin_Project/Program.cs
+++ Fin_Project/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, 
+// либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+string[] initialArr = { "Sunday", "Monday", "Tusday", "Wednesday", "Thur", "Friday", "Saturday" };
+
+string[] ChangeArr(string[] someArr)
+{
+    string[] resultArr = new string[someArr.Length];
+
+    for (int i = 0; i < resultArr.Length; i++)
+    {
+        if (someArr[i].Length <= 3)
+        {
+            resultArr[i] = someArr[i];
+        }
+    }
+
+    return resultArr;
+};
+
+string[] RemoveNullStr(string[] someArr)
+{
+    string[] resultArr = new string[someArr.Length];
+
+    for (int i = 0; i < resultArr.Length; i++)
+    {
+        if (someArr[i].Length != 0)
+        {
+            resultArr[i] = someArr[i];
+        }
+    }
+
+    return resultArr;
+}
+
+string[] changeArr = ChangeArr(initialArr);
+string[] finalArr = RemoveNullStr(changeArr);
+
+System.Console.Write($"Update array: [{string.Join(", ", finalArr)}]");
